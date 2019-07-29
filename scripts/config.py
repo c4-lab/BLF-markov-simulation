116d4e7672e9f8ce6aa6ffeeb5392cf83fbe2747
Niraj <sitaulaniraj@gmail.com>
2019-07-29 10:14:31 -0400

    Updated the scripts

---
+++ scripts/config.py
@@ -0,0 +1,13 @@
+# constants for creating coherence matrix 
+number_of_bits = 10
+seed_val = [0]
+coh_mat_min_value = 0.5
+coh_mat_max_value = 0.9
+
+
+# for Agents class
+global_local='local'
+scale = 0.8
+
+# other parameters
+num_agents = 100
