0de588066f2d01ea5a24669957b9e585f4a5ba74
Josh Introne <jeintron@syr.edu>
2020-02-19 10:34:15 -0500

    Some refactoring and modified coherence code

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -13,6 +13,60 @@ coh_network_file = "coherence.edgelist"
 embeddings_file = "_coherence.emb"
 
 
+
+def normalize_rows(m,noise=.001):
+    for i, row in enumerate(m):
+        for j, col in enumerate(row):
+            if m[i][j] ==0:
+                m[i][j] += noise
+
+        summing = sum(row)
+        for j, col in enumerate(row):
+            m[i][j] = m[i][j]/summing
+
+def hamming(a,b):
+    return(bin(a^b).count("1"))
+
+def init_coherence_matrix(nob,attractors,search_distance,inertial_weight=1,baseline = 1):
+    attrctr_space_vec = np.zeros(2**nob)+baseline
+    attractor_df = []
+    for a in attractors:
+        location = a[0]
+        depth = a[1]
+        radius = a[2]
+
+        for j in range(2**nob):
+            diff = hamming(j, location)
+            attractor_df.append({"attractor":a,"position":j,"depth":depth,"distance":diff,"inbasin":diff<=radius})
+            #diff = abs(j-k) ### Uncomment if needed to look into euclidean space
+            if diff <= radius:
+                attrctr_state_distance = (1-diff/radius)*depth
+                attrctr_space_vec[j] = max(attrctr_space_vec[j], attrctr_state_distance)
+
+    attrctr_space_mat = np.tile(attrctr_space_vec, (2**nob, 1))
+
+    pd.DataFrame(attractor_df).to_csv("attractors.csv")
+    with open("attractors.csv","a") as f:
+        line = "# inertia {}".format(inertial_weight)
+        f.write(line)
+
+    # create inertial matrix
+    inertia_matrix = np.zeros((2**nob, 2**nob))
+
+    for row_st, row in enumerate(inertia_matrix):
+        for col_st, col in enumerate(row):
+            bits_difference = hamming(row_st, col_st)
+            #         bits_difference = np.abs(row_st- col_st)
+            inertia_matrix[row_st, col_st] = max(0, 1- bits_difference *(1/search_distance))
+
+    coherence_matrix = attrctr_space_mat
+    if inertial_weight > 0:
+         coherence_matrix*=(inertial_weight*inertia_matrix)
+    normalize_rows(coherence_matrix,0)
+    pd.DataFrame(coherence_matrix).to_csv("coherence_matrix.csv")
+
+    return attrctr_space_mat,inertia_matrix,coherence_matrix
+
 def dump_coherence_matrix_to_edgelist(write = True):
     with open(results_file) as f:
         results = json.load(f)
